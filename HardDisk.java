e40bec5995cf2c364e3feed4d5fc654145c16cd9
SYW-github <72386619+SYW-github@users.noreply.github.com>
2020-10-07 21:21:24 +0800

    Add files via upload

---
+++ HardDisk.java
@@ -0,0 +1,29 @@
+package test2.java;
+
+class HardDisk {
+
+private int amount; 
+
+String type;
+
+String getType() {
+	return type;
+}
+
+void setType(String type) {
+	 this.type =type;
+}
+
+int getAmount() {
+
+return amount;
+
+}
+
+public void setAmount(int amount) {
+
+this.amount = amount;
+
+}
+
+}
---
+++ Test.java
@@ -0,0 +1,29 @@
+package test2.java;
+
+public class Test {
+
+public static void main(String args[]) {
+
+CPU cpu = new CPU();
+
+HardDisk HD=new HardDisk();
+
+cpu.setSpeed(2200);
+
+cpu.setname("intel");
+
+HD.setAmount(200);
+
+HD.setType("¹ÌÌ¬");
+
+PC pc =new PC();
+
+pc.setCPU(cpu);
+
+pc.setHardDisk(HD);
+
+pc.show();
+
+}
+
+}
